3314903ca8705af9ee510ab89a878ac590a0da16
tiltproofRain <srijannr1@gmail.com>
2020-05-09 13:58:05 +0530

    added keywords

--- src/Lexer/keywords.ts
+++ src/Lexer/keywords.ts
@@ -1,4 +1,16 @@
 let keywords = Object.freeze({
   'or': TokType.OR,
-  'and': TokType.AND
+  'and': TokType.AND,
+  'break': TokType.BREAK,
+  'if': TokType.IF,
+  'else': TokType.ELSE,
+  'elif': TokType.ELIF,
+  'true': TokType.TRUE,
+  'false': TokType.FALSE,
+  'func': TokType.FUNC,
+  'in': TokType.IN,
+  'for': TokType.FOR,
+  'do': TokType.DO,
+  'while': TokType.WHILE,
+  'const': TokType.CONST
 });
