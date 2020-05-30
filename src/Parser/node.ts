db183de0923c362b14b02ec1a5b6958fb953abb3
tiltproofRain <srijannr1@gmail.com>
2020-05-30 17:57:36 +0530

    nope, I'll have typechecking for sure

--- src/Parser/node.ts
+++ src/Parser/node.ts
@@ -1,13 +1,28 @@
 import Type = require("./nodetype");
 
 const ASTNode = {
-  [Type.BinaryExpr] : function() {
-    return {
+  [Type.BinaryExpr]: function () {
+    return node({
       left: null,
       right: null,
       op: null,
       kind: Type.BinaryExpr,
-    };
+    });
   },
-
+  [Type.PreUnaryExpr]: function () {
+    return node({
+      op: null,
+      operand: null,
+    });
+  },
 };
+
+function node(astNode) {
+  astNode.sourceInfo = {
+    start: -1,
+    end: -1,
+  };
+  return astNode
+}
+
+export = ASTNode
